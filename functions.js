e212cc61c7d0a98cf3c1c19240d5c2b652a54e2b
Derrick Mei <derrick.mei.168@gmail.com>
2019-08-18 02:53:14 +0800

    added attributes to elementCreator

--- functions.js
+++ functions.js
@@ -1,8 +1,12 @@
 function elementCreator(configObj) {
     const element = document.createElement(configObj.type);
-    if (configObj.classlist) element.classList.add([...configObj.classlist]);
+    if (configObj.classList) element.classList.add(...configObj.classList);
     if (configObj.textContent) element.textContent = configObj.textContent;
     if (configObj.parent) configObj.parent.appendChild(element);
-
+    if (configObj.attributes) {
+        configObj.attributes.forEach(attribute => {
+            element[attribute[0]] = attribute[1];
+        })
+    }
     return element;
 }
